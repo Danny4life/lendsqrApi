772ade1a02aaba013c01578e3bcbec028bfdd269
Segun Osiki <osiki.segun@gmail.com>
2023-01-24 07:16:36 +0100

    first commit

---
+++ src/main/java/com/osiki/lendsqrapi/LendsqrApiApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.lendsqrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendsqrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendsqrApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/osiki/lendsqrapi/entity/UserEntity.java
@@ -0,0 +1,17 @@
+//package com.osiki.lendsqrapi.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Data
+//@Entity
+//@Table(name = "users")
+//public class UserEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//
+//    long id;
+//    String email;
+//    String password;
+//}
