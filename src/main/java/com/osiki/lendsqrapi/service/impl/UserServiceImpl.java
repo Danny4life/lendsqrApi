eac5983b1daf51d9741a6939d7ec0432a7644fae
Segun Osiki <osiki.segun@gmail.com>
2023-01-24 07:39:08 +0100

    add user repository dependecy to user service impl class

--- src/main/java/com/osiki/lendsqrapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/osiki/lendsqrapi/service/impl/UserServiceImpl.java
@@ -1,8 +1,17 @@
 package com.osiki.lendsqrapi.service.impl;
 
+import com.osiki.lendsqrapi.repository.UserRepository;
 import com.osiki.lendsqrapi.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+
+    private UserRepository userRepository;
+
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
