dd69675a18bfc0e2130aa2deca20e82126011bc8
Segun Osiki <osiki.segun@gmail.com>
2023-01-24 07:22:11 +0100

    create User entity class

--- src/main/java/com/osiki/lendsqrapi/entity/UserEntity.java
+++ src/main/java/com/osiki/lendsqrapi/entity/UserEntity.java
@@ -1,17 +1,20 @@
-//package com.osiki.lendsqrapi.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//
-//@Data
-//@Entity
-//@Table(name = "users")
-//public class UserEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//
-//    long id;
-//    String email;
-//    String password;
-//}
+package com.osiki.lendsqrapi.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
+    long id;
+    String email;
+    String password;
+
+}
