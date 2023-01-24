22851bc7181cb0218a4dd1fc8c5699ffdcc1e7be
Segun Osiki <osiki.segun@gmail.com>
2023-01-24 07:35:10 +0100

    create User service implementation class

---
+++ src/main/java/com/osiki/lendsqrapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.osiki.lendsqrapi.service.impl;
+
+import com.osiki.lendsqrapi.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
