f365d94b4cbe18806d234d817cbf7f4d2528b105
Segun Osiki <osiki.segun@gmail.com>
2023-01-27 14:17:41 +0100

    create user controller method

--- src/main/java/com/osiki/lendsqrapi/service/UserDetailsService.java
+++ src/main/java/com/osiki/lendsqrapi/service/UserDetailsService.java
@@ -1,4 +1,7 @@
 package com.osiki.lendsqrapi.service;
 
+import com.osiki.lendsqrapi.model.UserDetails;
+
 public interface UserDetailsService {
+    UserDetails createUserDetails(UserDetails userDetails);
 }
--- src/main/java/com/osiki/lendsqrapi/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/osiki/lendsqrapi/service/impl/UserDetailsServiceImpl.java
@@ -1,8 +1,13 @@
 package com.osiki.lendsqrapi.service.impl;
 
+import com.osiki.lendsqrapi.model.UserDetails;
 import com.osiki.lendsqrapi.service.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails createUserDetails(UserDetails userDetails) {
+        return null;
+    }
 }
