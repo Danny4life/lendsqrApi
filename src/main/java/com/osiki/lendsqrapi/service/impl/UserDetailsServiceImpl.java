f6f3684b8c497dfca091e7a8405318c85179418e
Segun Osiki <osiki.segun@gmail.com>
2023-01-27 14:02:28 +0100

    create user details service implmentation class

---
+++ src/main/java/com/osiki/lendsqrapi/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.osiki.lendsqrapi.service.impl;
+
+import com.osiki.lendsqrapi.service.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+}
