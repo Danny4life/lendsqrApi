0a943e76d303c7e2ae3b9c5035334d194b3050f0
Segun Osiki <osiki.segun@gmail.com>
2023-01-27 14:21:02 +0100

    create object of user details entity

--- src/main/java/com/osiki/lendsqrapi/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/osiki/lendsqrapi/service/impl/UserDetailsServiceImpl.java
@@ -1,13 +1,29 @@
 package com.osiki.lendsqrapi.service.impl;
 
+import com.osiki.lendsqrapi.entity.UserDetailsEntity;
 import com.osiki.lendsqrapi.model.UserDetails;
+import com.osiki.lendsqrapi.repository.UserDetailsRepository;
 import com.osiki.lendsqrapi.service.UserDetailsService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserDetailsRepository userDetailsRepository;
+
+    public UserDetailsServiceImpl(UserDetailsRepository userDetailsRepository) {
+        this.userDetailsRepository = userDetailsRepository;
+    }
+
     @Override
     public UserDetails createUserDetails(UserDetails userDetails) {
-        return null;
+
+        UserDetailsEntity userDetailsEntity = new UserDetailsEntity();
+
+        BeanUtils.copyProperties(userDetails, userDetailsEntity);
+        userDetailsRepository.save(userDetailsEntity);
+
+        return userDetails;
     }
 }
