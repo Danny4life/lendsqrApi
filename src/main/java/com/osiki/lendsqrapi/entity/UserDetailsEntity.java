01e64ba696c9f5db2a2938949715625abcf08b88
Segun Osiki <osiki.segun@gmail.com>
2023-01-27 16:00:08 +0100

    create abstract base class

--- src/main/java/com/osiki/lendsqrapi/entity/UserDetailsEntity.java
+++ src/main/java/com/osiki/lendsqrapi/entity/UserDetailsEntity.java
@@ -5,25 +5,32 @@ import javax.persistence.*;
 import com.osiki.lendsqrapi.enums.Children;
 import com.osiki.lendsqrapi.enums.Gender;
 import com.osiki.lendsqrapi.enums.MaritalStatus;
+import com.osiki.lendsqrapi.model.Base;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "user_details_tbl")
-public class UserDetailsEntity {
+public class UserDetailsEntity extends Base {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private long id;
+//    private long id;
+
     private String fullName;
     private String phoneNumber;
     private String emailAdd;
     private String bvn;
+    private String type_of_residence;
     private Gender gender;
     private MaritalStatus maritalStatus;
     private Children children;
-    private String type_of_residence;
+
+//    private String gender;
+//    private String maritalStatus;
+//    private String children;
+
 
 
 }
--- src/main/java/com/osiki/lendsqrapi/model/UserDetails.java
+++ src/main/java/com/osiki/lendsqrapi/model/UserDetails.java
@@ -10,15 +10,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDetails {
+public class UserDetails extends Base{
 
-    private long id;
+//    private long id;
     private String fullName;
     private String phoneNumber;
     private String emailAdd;
     private String bvn;
+    private String type_of_residence;
     private Gender gender;
     private MaritalStatus maritalStatus;
     private Children children;
-    private String type_of_residence;
+//    private String gender;
+//    private String maritalStatus;
+//    private String children;
+
+
 }
