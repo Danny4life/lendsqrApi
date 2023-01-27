1190491376a452b084143da3aeaeca3104471cd1
Segun Osiki <osiki.segun@gmail.com>
2023-01-27 13:54:13 +0100

    created user details class

--- src/main/java/com/osiki/lendsqrapi/model/UserDetails.java
+++ src/main/java/com/osiki/lendsqrapi/model/UserDetails.java
@@ -1,4 +1,24 @@
 package com.osiki.lendsqrapi.model;
 
+import com.osiki.lendsqrapi.enums.Children;
+import com.osiki.lendsqrapi.enums.Gender;
+import com.osiki.lendsqrapi.enums.MaritalStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetails {
+
+    private long id;
+    private String fullName;
+    private String phoneNumber;
+    private String emailAdd;
+    private String bvn;
+    private Gender gender;
+    private MaritalStatus maritalStatus;
+    private Children children;
+    private String type_of_residence;
 }
