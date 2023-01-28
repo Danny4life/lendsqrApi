f02c05dc625b31070d04b5539a8cbe46d51f12cb
Segun Osiki <osiki.segun@gmail.com>
2023-01-28 11:09:06 +0100

    add cross orign annotaion

--- src/main/java/com/osiki/lendsqrapi/controller/UserDetailsController.java
+++ src/main/java/com/osiki/lendsqrapi/controller/UserDetailsController.java
@@ -2,11 +2,9 @@ package com.osiki.lendsqrapi.controller;
 
 import com.osiki.lendsqrapi.model.UserDetails;
 import com.osiki.lendsqrapi.service.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class UserDetailsController {
